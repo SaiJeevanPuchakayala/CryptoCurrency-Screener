8073f296a56deb659aa550f4b198410fc2040f84
saijeevanpuchakayala <saijeevan2002@gmail.com>
2021-07-06 08:51:19 +0530

    added text carousel for news

--- app.py
+++ app.py
@@ -3,15 +3,55 @@ import json
 import time
 from datetime import datetime
 from flask import Flask, render_template, Response
+from bs4 import BeautifulSoup
 
 app = Flask("__name__")
 
 API_URL = "https://yahoo-crypto-api.herokuapp.com/chart"
 
 
+class Scraper:
+    def __init__(self, keywords):
+        self.markup = requests.get('https://economictimes.indiatimes.com/markets/cryptocurrency').text
+        self.keywords = keywords
+
+    def parse(self):
+        soup = BeautifulSoup(self.markup, 'lxml')
+        links = soup.findAll('a', limit=300)  
+        self.news_links = []
+        self.news_linksTexts = []
+        for link in links:
+            for keyword in self.keywords:
+                if keyword in link.text and '?' not in link.text:
+                    self.news_links.append(link)
+        for link in self.news_links:
+            self.news_linksTexts.append(link.text)
+
+
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    s = Scraper([
+    'cryptocurrency',
+    'crypto',
+    'bitcoin',
+    'crash',
+    'cryptocurrency prices',
+    'government',
+    'ranks',
+    'towards',
+    'crash',
+    'market',
+    'trends',
+    'financials',
+    'capitalist'
+    ])
+
+    news_list = []
+    s.parse()
+    for x in s.news_linksTexts:
+        news_list.append(x)
+    return render_template("index.html",news=list(set(news_list)))
 
 
 @app.route('/table')
