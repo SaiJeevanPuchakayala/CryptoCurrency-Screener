f86c1aa71ccaec0d23db875ae935e31d65560ff8
saijeevanpuchakayala <saijeevan2002@gmail.com>
2021-07-15 22:40:07 +0530

    added styling

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 import requests
-import json
-import time
-from datetime import datetime
-from flask import Flask, render_template, Response
+from flask import Flask, render_template
 from bs4 import BeautifulSoup
 
 app = Flask("__name__")
-
-API_URL = "https://yahoo-crypto-api.herokuapp.com/chart"
 
 
 class Scraper:
@@ -61,29 +56,7 @@ def table_creator():
 
 @app.route('/chart')
 def chart_creator():
-    return render_template("chart2.html")   
-
-
-
-
-
-
-def generate_random_data(crypto_code):
-    while True:
-        price = float(requests.get(API_URL).json()[crypto_code]['price'].replace(",",''))
-        json_data = json.dumps(
-            {
-                "time": datetime.now().strftime("%H:%M:%S"),
-                "value": price,
-            }
-        )
-        yield f"data:{json_data}\n\n"
-        time.sleep(15)
-
-
-@app.route("/chart-data/<crypto>")
-def chart_data(crypto):
-    return Response(generate_random_data(crypto), mimetype="text/event-stream")
+    return render_template("chart.html")   
 
 
 if __name__=='__main__':
