aec47ab6ebc71443a2c4981b6d9e4be604a292c3
saijeevanpuchakayala <saijeevan2002@gmail.com>
2021-08-11 00:22:30 +0530

    fixes

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import requests
 from flask import Flask, render_template, request, jsonify
 from bs4 import BeautifulSoup
 import cfscrape
-import config
 from coinbase.wallet.client import Client
 from coinbase.wallet.model import APIObject
 
